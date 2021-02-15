92f44685e32d47530cc15b5784f18030cb0d84b5
KleachiM <65865966+KleachiM@users.noreply.github.com>
2021-02-15 14:04:38 +0300

    Update new.py

--- new.py
+++ new.py
@@ -1,23 +1,42 @@
 import requests
 from pprint import pprint
+import sys
+import xlwings
 import os
-import xlwings as xw
 
-URL_1 = 'https://fgis.gost.ru/fundmetrology/cm/icdb/vri/select?fq=verification_year:2021&fq=mi.mitype:*8508*&fq=verification_date:[2021-01-20T00:00:00Z%20TO%202021-02-01T23:59:59Z]&q=*&fl=vri_id,org_title,mi.mitnumber,mi.mititle,mi.mitype,mi.modification,mi.number,verification_date,valid_date,applicability,result_docnum&sort=verification_date+desc,org_title+asc&rows=20&start=0'
+#
+# URL = 'https://fgis.gost.ru/fundmetrology/cm/results'
+#
+# params = {
+#     'filter_mi_mitype': '8508',
+#     'filter_verification_date_start': '2021-01-20',
+#     'filter_verification_date_end': '2021-01-01'
+# }
+# URL = 'https://fgis.gost.ru/fundmetrology/cm/icdb/vri/select?fq=verification_year:2021&\
+# fq=mi.mitype:*8508*&fq=verification_date:[2021-01-10T00:00:00Z%20TO%202021-02-01T23:59:59Z]&\
+# q=*&fl=vri_id,org_title,mi.mitnumber,mi.mititle,mi.mitype,mi.modification,mi.number,verification_date,valid_date,applicability,result_docnum&\
+# sort=verification_date+desc,org_title+asc&rows=50000'
 
-proxies = {
-  'http': 'http://gate.inet:3128',
-  'https': 'http://gate.inet:3128',
-}
-
-resp = requests.get(URL_1, proxies=proxies)
-resp_json = resp.json()
-# pprint(resp_json)
 file_name = 'test.xlsx'
 full_path = os.path.join(os.getcwd(), file_name)
 wb = xw.Book(full_path)
 sh = wb.sheets['Лист1']
-print(sh.range('A1').value)
-sh.range("A2").value = resp_json['response']['docs'][0]['mi.number']
+
+try:
+    resp = requests.get(URL)
+except requests.exceptions.ConnectionError:
+    print('Нет соединения с сервером')
+    sys.exit(0)
+
+resp_json = resp.json()
+
+headers = resp_json['responseHeader']
+
+print(resp_json['response']['numFound'])
+
+# URL1 = 'https://fgis.gost.ru/fundmetrology/cm/icdb/vri/select'
+# resp = requests.get(URL1, headers=headers)
+# print(resp)
+
 # wb.save()
 # xw.apps.active.quit()
