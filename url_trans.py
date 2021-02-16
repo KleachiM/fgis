6ff44333dcf550cb33df9a5e2c9b2e7dcbb414d5
KleachiM <65865966+KleachiM@users.noreply.github.com>
2021-02-16 07:52:56 +0300

    Update url_trans.py

--- url_trans.py
+++ url_trans.py
@@ -1,7 +1,7 @@
-rom typing import Tuple
+from typing import Tuple
 from urllib.parse import urlparse, urlunparse
 from pprint import pprint
-
+import re
 
 
 # URL = 'https://fgis.gost.ru/fundmetrology/cm/icdb/vri/select?fq=verification_year:2021&\
@@ -14,7 +14,7 @@ from pprint import pprint
 # print(len(obj))
 # print('============')
 # pprint(obj.query.split('&'))
-from date_trans import get_date, DAYS_COUNT
+from date_trans import get_date
 
 
 def make_url(mitnumber, number, date_from, date_to, verification_year):
@@ -25,7 +25,7 @@ def make_url(mitnumber, number, date_from, date_to, verification_year):
     list_for_query = [
         f'fq=verification_year:{verification_year}',
         f'fq=mi.mitnumber:*{mitnumber}*',
-        f'fq=mi.number:*{number}*',
+        f'fq=mi.number:*{add_slashes(number)}*',
         f'fq=verification_date:[{date_from}T00:00:00Z%20TO%20{date_to}T23:59:59Z]',
         'q=*',
         'fl=vri_id,org_title,mi.mitnumber,mi.mititle,mi.mitype,mi.modification,mi.number,verification_date,valid_date,applicability,result_docnum',
@@ -42,7 +42,19 @@ def make_url(mitnumber, number, date_from, date_to, verification_year):
     return urlunparse(created_url)
 
 
-dates = get_date(DAYS_COUNT)
-print(make_url(r'25984\-08', '139661207', dates['date_from'], dates['date_to'], dates['verification_year']))
-URL_FGIS = 'https://fgis.gost.ru/fundmetrology/cm/icdb/vri/select?fq=verification_year:2021&fq=mi.mitnumber:*25984\-08*&fq=mi.number:*139661207*&fq=verification_date:[2021-01-20T00:00:00Z%20TO%202021-02-14T23:59:59Z]&q=*&fl=vri_id,org_title,mi.mitnumber,mi.mititle,mi.mitype,mi.modification,mi.number,verification_date,valid_date,applicability,result_docnum&sort=verification_date+desc,org_title+asc&rows=20&start=0'
-print(URL_FGIS)
+def add_slashes(text):
+    pattern = re.compile(r'\W')
+
+    text = text.split()[0]  # убирает пробелы
+    new_text = ''
+    for char in text:
+        match = re.findall(pattern, char)
+        if match:
+            char = f'\\{match[0]}'
+        new_text = new_text + char
+    return new_text
+dates = get_date(30)
+print(make_url('216690-97', '016806', dates['date_from'], dates['date_to'], dates['verification_year']))
+# URL_FGIS = 'https://fgis.gost.ru/fundmetrology/cm/icdb/vri/select?fq=verification_year:2021&fq=mi.mitnumber:*25984\-08*&fq=mi.number:*139661207*&fq=verification_date:[2021-01-20T00:00:00Z%20TO%202021-02-14T23:59:59Z]&q=*&fl=vri_id,org_title,mi.mitnumber,mi.mititle,mi.mitype,mi.modification,mi.number,verification_date,valid_date,applicability,result_docnum&sort=verification_date+desc,org_title+asc&rows=20&start=0'
+# print(URL_FGIS)
+
